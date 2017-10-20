9d3fb556c33a2e2365976231b6c604b10825e8ba
Brett Beekley <brettbeekley@gmail.com>
2017-10-20 16:00:06 -0700

    Send response on off request

--- server.py
+++ server.py
@@ -107,6 +107,7 @@ def Off ():
         for i in range(strip.numPixels()):
             strip.setPixelColor(i, Color(0, 0, 0))
             strip.show()
+    return "Strip off"
 
 
 @app.route("/strandtest", methods=['GET'])
