5e2b488e38225bebcd4f0f961ba14ac9fe2cabae
Brett Beekley <brettbeekley@gmail.com>
2017-10-20 16:14:44 -0700

    Break animations into files in programs directory

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import time
 import sys
 from neopixel import *
 from flask import (Flask, request, send_from_directory)
+import programs.strandTest as strandTest
 
 ################
 # Strip Config #
@@ -18,62 +19,7 @@ LED_INVERT     = False   # True to invert the signal (when using NPN transistor 
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 
-# Define functions which animate LEDs in various ways.
-def colorWipe(strip, color, wait_ms=50):
-    """Wipe color across display a pixel at a time."""
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, color)
-        strip.show()
-        time.sleep(wait_ms/1000.0)
 
-def theaterChase(strip, color, wait_ms=50, iterations=10):
-    """Movie theater light style chaser animation."""
-    for j in range(iterations):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, color)
-            strip.show()
-            time.sleep(wait_ms/1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, 0)
-
-def wheel(pos):
-    """Generate rainbow colors across 0-255 positions."""
-    if pos < 85:
-        return Color(pos * 3, 255 - pos * 3, 0)
-    elif pos < 170:
-        pos -= 85
-        return Color(255 - pos * 3, 0, pos * 3)
-    else:
-        pos -= 170
-        return Color(0, pos * 3, 255 - pos * 3)
-
-def rainbow(strip, wait_ms=20, iterations=1):
-    """Draw rainbow that fades across all pixels at once."""
-    for j in range(256*iterations):
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, wheel((i+j) & 255))
-        strip.show()
-        time.sleep(wait_ms/1000.0)
-
-def rainbowCycle(strip, wait_ms=20, iterations=5):
-    """Draw rainbow that uniformly distributes itself across all pixels."""
-    for j in range(256*iterations):
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
-        strip.show()
-        time.sleep(wait_ms/1000.0)
-
-def theaterChaseRainbow(strip, wait_ms=50):
-    """Rainbow movie theater light style chaser animation."""
-    for j in range(256):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, wheel((i+j) % 255))
-            strip.show()
-            time.sleep(wait_ms/1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, 0)
 
 #################
 # Server Config #
@@ -122,21 +68,21 @@ def StrandTest ():
     if animation == 'colorWipe':
         # if no color provided
         if color == '':
-            colorWipe(strip, Color(255, 0, 0)) # Red wipe
-            colorWipe(strip, Color(0, 255, 0)) # Blue wipe
-            colorWipe(strip, Color(0, 0, 255)) # Green wipe
+            strandTest.colorWipe(strip, Color(255, 0, 0)) # Red wipe
+            strandTest.colorWipe(strip, Color(0, 255, 0)) # Blue wipe
+            strandTest.colorWipe(strip, Color(0, 0, 255)) # Green wipe
     elif animation == 'theaterChase':
         # if no color provided
         if color == '':
-            theaterChase(strip, Color(127, 127, 127))  # White theater chase
-            theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-            theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+            strandTest.theaterChase(strip, Color(127, 127, 127))  # White theater chase
+            strandTest.theaterChase(strip, Color(127,   0,   0))  # Red theater chase
+            strandTest.theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
     elif animation == 'rainbow':
-        rainbow(strip)
+        strandTest.rainbow(strip)
     elif animation == 'rainbowCycle':
-        rainbowCycle(strip)
+        strandTest.rainbowCycle(strip)
     elif animation == 'theaterChaseRainbow':
-        theaterChaseRainbow(strip)
+        strandTest.theaterChaseRainbow(strip)
     else:
         return "Invalid request"
 
