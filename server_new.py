269bbefdeb3a4f130681ead14dabd51b76b1f160
Brett Beekley <brettbeekley@gmail.com>
2018-04-18 19:44:52 -0700

    Genericize route

--- server_new.py
+++ server_new.py
@@ -1,22 +1,21 @@
 
 from flask import (Flask, request)
 import programs.strandTest as strandTest
-# import programs.off as Off
+import programs.off as off
 import programs.hue as hue
-# import programs.helpers as helpers
 import controller
 
 APP = Flask(__name__, static_folder='client')
 STRIP = controller.STRIP
 PROGRAMS = {
     "hue": hue,
-    "strandTest": strandTest
+    "strandTest": strandTest,
+    "off": off
 }
 
 @APP.route("/program/<string:program>/<string:pattern>", methods=['POST'])
 def genericProgramRoute(program, pattern):
     progParams = request.get_json()
-    print(PROGRAMS[program].__dict__)
     controller.push([PROGRAMS[program].__dict__.get(pattern), STRIP] + progParams)
     return "Program running"
 
